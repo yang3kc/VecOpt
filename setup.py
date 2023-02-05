b1611ac19bf91715c9b9817d7b1ad727a599a4bb
Kaicheng Yang <yangkc@iu.edu>
2023-02-04 21:36:30 -0500

    add: add package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="vecopt",
+    version="0.1",
+    description="Infer moral intuitions from text",
+    url="https://github.com/yangkcatiu/VecOpt",
+    author="Kai-Cheng Yang <yang3kc@gmail.com>",
+    license="MIT",
+    packages=["VecOpt"],
+    install_requires=["pandas"],
+)
